a6cb02ef9d9c2818a3ab84e419e7981b42adbbb9
kjoshi1988 <kjoshi1988@gmail.com>
2020-05-15 12:40:33 +0530

    Renamed to vormir js

---
+++ BrocFile.js
@@ -0,0 +1,63 @@
+// Brocfile.js
+const funnel = require('broccoli-funnel');
+const merge = require('broccoli-merge-trees');
+const compileSass = require('broccoli-sass-source-maps')(require('sass'));
+const Rollup = require("broccoli-rollup");
+const babel = require("rollup-plugin-babel");
+const nodeResolve = require('rollup-plugin-node-resolve');
+const commonjs = require('rollup-plugin-commonjs');
+
+const appRoot = "app";
+
+// Copy HTML file from app root to destination
+const html = funnel(appRoot, {
+  files: ["index.html"],
+  annotation: "Index file",
+});
+
+// Compile JS through rollup
+let js = new Rollup(appRoot, {
+  inputFiles: ["**/*.js"],
+  annotation: "JS Transformation",
+  rollup: {
+    input: {
+      app: "app.js"
+    },
+    output: {
+      file: "assets/app.js",
+      format: "iife",
+      sourcemap: true,
+    },
+    plugins: [
+      nodeResolve({
+        jsnext: true,
+        browser: true,
+      }),
+      commonjs({
+        include: 'node_modules/**',
+      }),
+      babel({
+        exclude: "node_modules/**",
+      }),
+    ],
+  }
+});
+
+// Copy CSS file into assets
+const css = compileSass(
+    [appRoot],
+    'styles/app.scss',
+    'assets/app.css',
+    {
+      sourceMap: true,
+      sourceMapContents: true,
+      annotation: "Sass files"
+    }
+);
+
+// Copy public files into destination
+const public = funnel('public', {
+  annotation: "Public files",
+});
+
+module.exports = merge([html, js, css, public], {annotation: "Final output"});
